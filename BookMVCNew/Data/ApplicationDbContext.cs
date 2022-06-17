3f9a4fff28220a966d355130334dd52cf28da499
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-17 13:55:37 -0600

    Initial Commit

---
+++ BookMVCNew/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookMVCNew.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookMVCNew.Data
+{
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
