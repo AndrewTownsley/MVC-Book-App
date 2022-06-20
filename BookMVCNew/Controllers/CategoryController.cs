92b0c531add4c24ab57d8b586843ae545ca7ad34
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-20 15:05:49 -0600

    Added category model, added server-side validation

--- BookMVCNew/Controllers/CategoryController.cs
+++ BookMVCNew/Controllers/CategoryController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookMVCNew.Data;
+using BookMVCNew.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookMVCNew.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        } 
+        
+        // Get
+        public IActionResult Create()
         {
             return View();
         }
+        
+        // Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 }
